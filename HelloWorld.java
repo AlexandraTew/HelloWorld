13b5da64ca99098ef9a1c5d3a5c5743498c8679d
AlexandraTew <135042292+AlexandraTew@users.noreply.github.com>
2023-08-23 14:48:36 -0400

    Dice Roll Added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 public class HelloWorld {
 
     public static void main(String[] args){
         
-        System.out.println("Hello World  oe!");
+        System.out.println("Hello World!");
 
-    } 
-    
+        // Simulate a dice roll
+        int diceValue = rollDice();
+        System.out.println("You rolled a " + diceValue);
+    }
+         // Function to simulate a dice roll
+    public static int rollDice() {
+        Random random = new Random();
+        return random.nextInt(6) + 1; // Generates a random number from 1 to 6
+    }
+
 }
